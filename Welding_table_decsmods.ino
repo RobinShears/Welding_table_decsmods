318d4800bf9480e0fbaeaa382a04d38add600b0e
PanGalacticTech <dec.h38@gmail.com>
2020-08-06 17:18:37 +0100

    Updatad for smoother operation

--- Welding_table_decsmods.ino
+++ Welding_table_decsmods.ino
@@ -1,7 +1,10 @@
 //Code that controls the speed and direction
 //of the welding table at skyrora
 //Robin Shears
-int SpeedIn; //pot middle pin
+
+
+int SpeedIn; //pot read value
+int speedPot = A6; //pot read value
 int PWM = 5; // outputs variable sqaure wave for speed
 int DIRSwitch = 3; //switch to switch direction
 int DIRon = 2; //always on
@@ -12,13 +15,14 @@ uint32_t Sqauresp;
 int lastSpeed;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(SpeedIn, INPUT);
-  pinMode(PWM, OUTPUT);
+  Serial.begin(115200);
+
+  pinMode(speedPot, INPUT);
   pinMode(DIRSwitch, INPUT);
+
+  pinMode(PWM, OUTPUT);
   pinMode(DIR1, OUTPUT);
   pinMode(DIR2, OUTPUT);
-  pinMode(SpeedIn, INPUT);
   pinMode(DIRon, OUTPUT);
   pinMode(pulseV, OUTPUT);
   digitalWrite(DIRon, HIGH);
@@ -27,60 +31,81 @@ void setup() {
 }
 
 
+
+
+uint32_t previousTime;
+
+
 void loop() {
 
-  
+  directionSwitch();
 
-  if (digitalRead(DIRSwitch) == HIGH) {   //controls the direction of the table based on the switch position
-    digitalWrite(DIR2, LOW);
-  }
-  else {
-    digitalWrite(DIR2, HIGH);
-  }
-
-
-  SpeedIn = analogRead(A6);
+  SpeedIn = analogRead(speedPot);
   Serial.print("Speed in: ");
   Serial.print(SpeedIn);
   Serial.print("   ");
 
-  if (SpeedIn > lastSpeed+2 || SpeedIn < lastSpeed-2){
-  Sqauresp = map(SpeedIn, 0, 1024, 312, 3850000);
+  if (SpeedIn > lastSpeed + 1 || SpeedIn < lastSpeed - 1) {
+    Sqauresp = map(SpeedIn, 0, 1024, 312, 38500);
+    lastSpeed = SpeedIn;
   }
 
-  
+
   Serial.print("Squaresp: ");
-  
-  Serial.println(Sqauresp);
+
+  Serial.print(Sqauresp);
   Serial.println(" ");
-  //Serial.println(" ");       
-  
-  for (int x = 0; x < 10; x++) {
-    digitalWrite(PWM, HIGH); //genorates a pwm waveform at the frequency needed compared to the input
-    delayMicroseconds(Sqauresp);
-    digitalWrite(PWM, LOW);
-    delayMicroseconds(Sqauresp); //(min ms+(range of ms/1024)*speedin)
+  //Serial.println(" ");
+
+
+
+  uint32_t currentTime = micros();
+
+  if (currentTime - previousTime >= Sqauresp*100) {
+
+    if (PWM == HIGH) {
+      digitalWrite(PWM, LOW);
+    } else {
+      digitalWrite(PWM, HIGH);
+    }
+
+
+    previousTime = currentTime;
   }
-  //use 7 segment library here will update every ten pulses to releave the control circuit
-  //print(calib*Speedin);
 
 
 
-lastSpeed = SpeedIn;
+//use 7 segment library here will update every ten pulses to releave the control circuit
+//print(calib*Speedin);
+
+
+
+
+
+}
+
+
+
+
+
+void directionSwitch() {
+
+  if (digitalRead(DIRSwitch) == HIGH) {   //controls the direction of the table based on the switch position
+    digitalWrite(DIR2, LOW);
+    
+  }
+  else {
+    digitalWrite(DIR2, HIGH);
+    Serial.println("DIRswitch = LOW");
+  }
 
 }
 
 /*
-if (whateveveryoucallit.delayScript(delayTime){
- // do the thing
-}
-/*
- * 
- * // delayTime = squareSP
-currentTime = micros();
+  if (whateveveryoucallit.delayScript(delayTime){
+  // do the thing
+  }
+  /*
 
-if (currentTime - previousTime >= delayTime){
-// do the thing
-previousTime = currentTime;
-}
+
 */
